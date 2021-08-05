92ada10422b64ff529edd6416fed720ff9e58aa4
letz0703 <letz0703@gmail.com>
2021-08-05 11:06:36 +0900

    Update MainActivity.java
    Register VM

--- app/src/main/java/com/letz/photomemory/MainActivity.java
+++ app/src/main/java/com/letz/photomemory/MainActivity.java
@@ -1,14 +1,29 @@
 package com.letz.photomemory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyImagesViewModel myImagesViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImagesViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication())
+                .create(MyImagesViewModel.class);
+        myImagesViewModel.getAllImages().observe(MainActivity.this, new Observer<List<MyImages>>() {
+            @Override
+            public void onChanged(List<MyImages> myImages) {
+
+            }
+        });
     }
 }
